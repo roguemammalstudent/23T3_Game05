6f1b10796c622d0e7038248be0fc6be0f93e4337
roguemammalstudent <135566059+roguemammalstudent@users.noreply.github.com>
2023-10-27 15:19:52 +1100

    Fixed level progression
    Level Progression was fixed (largely by Alex) -- one major issue is that the player was never actually tagged as "Player".

--- Assets/Scripts/AdvanceLevel.cs
+++ Assets/Scripts/AdvanceLevel.cs
@@ -5,27 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class AdvanceLevel : MonoBehaviour
 {
-    public GameObject wall;
-    public bool isDone;
+    //public GameObject wall;
+    //public bool isDone;
 
     private void Update()
     {
-        if (isDone == true)
-        {
-            wall.SetActive(false);
-        }
+        //if (isDone == true)
+        //{
+        //    wall.SetActive(false);
+        //}
 
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player")) // And bool spelt correct
         {
             AdvanceToLevel();
         }
     }
     public void AdvanceToLevel()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
